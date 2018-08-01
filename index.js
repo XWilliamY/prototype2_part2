10ddbd8ca185a13d2ebf80b14e8870e87b39226c
William <xwilliamy@gmail.com>
2018-08-01 05:11:21 -0400

    confirmSignIn isn't sending a confirmation code through SMS for some reason

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+
 import Amplify from 'aws-amplify';
 import config from './src/aws-exports';
 import {name as appName} from './app.json';
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 import {
   View,
   Text,
@@ -9,10 +9,13 @@ import {
 } from 'react-native'
 import { USER_KEY } from './config';
 
+import {Auth} from 'aws-amplify';
+
 export default class SignIn extends Component{
   state = {
-    username: '',
-    password: ''
+    username: '', password: '', 
+    user: {}, 
+    authenticationCode: '', showConfirmationForm: false
   }
   onChangeText = (key, value) => {
     this.setState({ [key]: value })
@@ -20,43 +23,76 @@ export default class SignIn extends Component{
   signIn = async () => {
     const {username, password} = this.state
     try {
-      // login logic
-      const user = await AsyncStorage.setItem(USER_KEY, username)
+      const user = await Auth.signIn(username, password)
       console.log('user successfully signed in!', user)
-      this.props.navigation.navigate('Home')
+      this.setState({ user, showConfirmationForm: true })
+      console.log(user)
     } catch(err){
       console.log('error', err)
+      alert(err)
     }
   }
 
+  confirmSignIn = async () => {
+    const { user, authenticationCode } = this.state
+    try {
+       await Auth.confirmSignIn(user, authenticationCode, 'SMS')
+       console.log('user successfully signed in!', user)
+       this.props.navigation.navigate('Home')
+    } catch (err) {
+      console.log('error:', err)
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          placeholder='Username'
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholderTextColor='white'
-          onChangeText={val => this.onChangeText('username', val)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder='Password'
-          autoCapitalize="none"
-          secureTextEntry={true}
-          placeholderTextColor='white'
-          onChangeText={val => this.onChangeText('password', val)}
-        />
-        <Button
-          title='Sign In'
-          onPress={this.signIn}
-        />
+        {
+          !this.state.showConfirmationForm && (
+            <Fragment>
+              <TextInput
+                style={styles.input}
+                placeholder='Username'
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholderTextColor='white'
+                onChangeText={val => this.onChangeText('username', val)}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder='Password'
+                autoCapitalize="none"
+                secureTextEntry={true}
+                placeholderTextColor='white'
+                onChangeText={val => this.onChangeText('password', val)}
+              />
+              <Button
+                title='Sign In'
+                onPress={this.signIn}
+              />
+            </Fragment>
+          )
+        }
+        {
+          this.state.showConfirmationForm && (
+            <Fragment>
+              <TextInput
+                style={styles.input}
+                placeholder='Authentication Code'
+                autoCapitalize="none"
+                placeholderTextColor='white'
+                onChangeText={val => this.onChangeText('authenticationCode', val)}
+              />
+              <Button
+                title='Confirm Sign In'
+                onPress={this.confirmSignIn}
+              />
+            </Fragment>
+          )
+        }        
       </View>
     )
   }
 }
-
 
 const styles = StyleSheet.create({
   input: {
