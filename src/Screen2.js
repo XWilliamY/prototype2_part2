40e7372068b168ddbbc9ed55077c38a80ef9c3f8
William <xwilliamy@gmail.com>
2018-08-02 12:23:28 -0400

    Almost broke this project with adding graphql and apollo, but we good now

--- src/Screen2.js
+++ src/Screen2.js
@@ -6,12 +6,36 @@ import {
   StyleSheet,
 } from 'react-native';
 
+import { Auth } from 'aws-amplify'
+import gql from 'graphql-tag';
+ 
+const query = gql`
+  {
+    user(id: 5) {
+      firstName
+      lastName
+    }
+  }
+`
+
 
 class Screen2 extends Component {
+  state = {
+    username: ''
+  }
+  async componentDidMount() {
+    Auth.currentUserInfo()
+      .then(data => {
+        this.setState({
+          username: data.username
+        })
+      })
+      .catch(err => console.log('error: ', err))
+  }
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Screen2</Text>
+        <Text>Hey {this.state.username}, this is screen222222!</Text>
       </View>
     );
   }
